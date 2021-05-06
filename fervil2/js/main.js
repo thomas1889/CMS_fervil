3f80530ab3ce521e4572377d736c6bdf39506b74
Santiago Barrios H <santiagobarrios08@gmail.com>
2021-05-06 22:39:28 +0200

    there are some minor changes to the interface

--- fervil2/js/main.js
+++ fervil2/js/main.js
@@ -2,7 +2,7 @@ const height = document.body.scrollHeight - window.innerHeight;
 const background = document.getElementById('background');
 
 window.onscroll = () => {
-  const widthBackground = (window.pageYOffset / height) * 350;
+  const widthBackground = (window.pageYOffset / height) * 700;
   if (widthBackground<=100) {
     background.style.width = widthBackground + '%';
   }
